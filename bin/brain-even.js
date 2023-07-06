6823cf82bc483f4364338fb68f08568f6ccdd61e
keka <kvtka4enko@yandex.ru>
2023-07-06 18:59:18 +0300

    modify brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,18 +1,22 @@
 import readlineSync from 'readline-sync';
 import greeting from '../src/cli.js';
 
-/*console.log('Welcome to the Brain Games!');
-greeting();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');*/
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 const readAnswer = () => {
   const number = Math.floor(Math.random() * 20);
   console.log(`Question: ${number}`);
   const answer = readlineSync.question('Your answer: ');
   if ((answer === 'yes' && number % 2 === 0) || (answer === 'no' && number % 2 !== 0)) {
-    return console.log('Correct!');
+    console.log('Correct!');
+  } else if (answer === 'yes' && number % 2 !== 0) {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'./nLet's try again, ${name}!`);
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'./nLet's try again, ${name}!`);
   }
-  return console.log('wrong');
 };
 
 readAnswer();
