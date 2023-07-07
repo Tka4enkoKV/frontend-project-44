ea23d51167525eb18db2fe388471c77b0b4df278
keka <kvtka4enko@yandex.ru>
2023-07-07 09:50:30 +0300

    finish brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import greeting from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
@@ -7,18 +7,23 @@ console.log(`Hello, ${name}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 const readAnswer = () => {
-  const number = Math.floor(Math.random() * 20);
-  console.log(`Question: ${number}`);
-  const answer = readlineSync.question('Your answer: ');
-  if ((answer === 'yes' && number % 2 === 0) || (answer === 'no' && number % 2 !== 0)) {
-    console.log('Correct!');
-  } else if (answer === 'yes' && number % 2 !== 0) {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'./nLet's try again, ${name}!`);
-  } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'./nLet's try again, ${name}!`);
+  let win = 0;
+  while (win < 3) {
+    const number = Math.floor(Math.random() * 20);
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+    if ((answer === 'yes' && number % 2 === 0) || (answer === 'no' && number % 2 !== 0)) {
+      console.log('Correct!');
+      win += 1;
+    } else if (answer === 'yes' && number % 2 !== 0) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'./nLet's try again, ${name}!`);
+      win = 0;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'./nLet's try again, ${name}!`);
+      win = 0;
+    }
   }
+  console.log(`Congratulations, ${name}!`);
 };
 
 readAnswer();
-
-// счетчик игр запускает луп, в котором если победы меньше 3, то игра. Если не угадал счетчик 0, победа +1
