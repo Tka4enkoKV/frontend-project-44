c781b3420da3e8b1d5ba7ed5b62d0ae8bd35d7b2
keka <kvtka4enko@yandex.ru>
2023-07-10 22:15:56 +0300

    change brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,29 +1,42 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import greeting from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const generateRandomNumber = () => Math.floor(Math.random() * 100);
 
-const readAnswer = () => {
+const askQuestion = (number) => {
+  console.log(`Question: ${number}`);
+  return readlineSync.question('Your answer: ');
+};
+
+const isEven = (number) => number % 2 === 0;
+
+const checkAnswer = (answer, number, name) => {
+  const isNumberEven = isEven(number);
+  if ((answer === 'yes' && isNumberEven) || (answer === 'no' && !isNumberEven)) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isNumberEven ? 'yes' : 'no'}'.\nLet's try again, ${name}!`);
+  return false;
+};
+
+const playGame = () => {
+  const name = greeting();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
   let win = 0;
   while (win < 3) {
-    const number = Math.floor(Math.random() * 20);
-    console.log(`Question: ${number}`);
-    const answer = readlineSync.question('Your answer: ');
-    if ((answer === 'yes' && number % 2 === 0) || (answer === 'no' && number % 2 !== 0)) {
-      console.log('Correct!');
+    const number = generateRandomNumber();
+    const answer = askQuestion(number);
+    const isCorrect = checkAnswer(answer, number, name);
+
+    if (isCorrect) {
       win += 1;
-    } else if (answer === 'yes' && number % 2 !== 0) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'./nLet's try again, ${name}!`);
-      win = 0;
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'./nLet's try again, ${name}!`);
       win = 0;
     }
   }
   console.log(`Congratulations, ${name}!`);
 };
 
-readAnswer();
+playGame();
